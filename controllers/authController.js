e015e800841ffb82dac618d651bad546e9c35574
Carlos <carlosarmando99@hotmail.com>
2023-07-11 21:27:41 -0400

    FIX

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
-import { comparePassword, hashPassword } from "../helpers/authHelper.js";
-import userModel from "../models/userModel.js"
+import { comparePassword, hashPassword } from "./helpers/authHelper.js";
+import userModel from "./models/userModel.js"
 import JWT from "jsonwebtoken"
 
 export const registerController = async (req,res) => {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
+
 import {
     registerController,
     loginController,
     testController,
 } from '../controllers/authController.js'
-import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+
+import { isAdmin, requireSignIn } from "../controllers/middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router()
-
+/*
 //routing
 //REGISTER || METHOD POST
 router.post('/register', registerController)
@@ -23,5 +25,5 @@ router.get('/test', requireSignIn, isAdmin, testController)
 router.get('/user-auth', requireSignIn, (req,res) => {
     res.status(200).send({ ok: true });
 })
-
+*/
 export default router
